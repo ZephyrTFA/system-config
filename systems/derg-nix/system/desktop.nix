c549d4474518ebd1e85d325cb6982211c69928d9
ZephyrTFA (Matthew) <matthew@tfaluc.com>
2025-03-05 18:55:05 -0500

    use gnome

--- systems/derg-nix/system/desktop.nix
+++ systems/derg-nix/system/desktop.nix
@@ -1,7 +1,7 @@
 {...}: {
-  services = {
-    xserver.enable = true;
-    displayManager.sddm.enable = true;
-    desktopManager.plasma6.enable = true;
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
   };
 }
