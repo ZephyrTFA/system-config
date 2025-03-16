788194689ffec22e506ee90a028e4fe62803bfef
ZephyrTFA (Matthew) <matthew@tfaluc.com>
2025-03-16 00:09:59 -0400

    forgor clang

--- users/derg.nix
+++ users/derg.nix
@@ -67,7 +67,10 @@
 
     home.file.".cargo/config.toml".text = ''
       [target.x86_64-unknown-linux-gnu]
-      linker = "clang"
+      linker = "${pkgs.clang}/bin/clang"
+      rustflags = ["-C", "link-arg=-fuse-ld=${pkgs.mold}/bin/mold"]
+      [target.i686-unknown-linux-gnu]
+      linker = "${pkgs.clang}/bin/clang"
       rustflags = ["-C", "link-arg=-fuse-ld=${pkgs.mold}/bin/mold"]
     '';
 
