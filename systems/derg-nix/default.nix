521b613004839716ce3af02c169f2795cd85f931
ZephyrTFA (Matthew) <matthew@tfaluc.com>
2025-06-20 12:04:40 -0400

    add wine and winetricks

--- systems/derg-nix/default.nix
+++ systems/derg-nix/default.nix
@@ -8,6 +8,8 @@
   virtualisation.podman.enable = true;
   programs.nix-ld.enable = true;
   environment.systemPackages = with pkgs; [
+    wineWowPackages.staging
+    winetricks
     alejandra
     direnv
     podman
