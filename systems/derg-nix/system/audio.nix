28a91880c8869dc99ef4bebcfe51a00d1b733a0b
ZephyrTFA (Matthew) <matthew@tfaluc.com>
2025-08-15 07:46:20 -0400

    disable pulseaudio preference

--- systems/derg-nix/system/audio.nix
+++ systems/derg-nix/system/audio.nix
@@ -15,11 +15,10 @@
   services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
     "monitor.bluez.properties" = {
       "bluez5.enable-sbc-xq" = true;
-      "bluez5.enable-msbc" = true;
+      "bluez5.enable-msbc" = false;
       "bluez5.enable-hw-volume" = true;
       "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
       "bluez5.codecs" = ["ldac" "aptx" "aptx_hd" "aac" "sbc"];
     };
   };
-  hardware.pulseaudio.enable = false;
 }
