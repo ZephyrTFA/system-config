bfff5663af4bbd1967125577302d6557f3087184
ZephyrTFA (Matthew) <matthew@tfaluc.com>
2025-08-15 07:47:04 -0400

    swap nvidia to both open source kernel and vulkan beta driver

--- systems/derg-nix/system/nvidia.nix
+++ systems/derg-nix/system/nvidia.nix
@@ -1,18 +1,12 @@
-{...}: {
-  hardware.graphics = {
-    enable = true;
-  };
-  services.xserver.videoDrivers = [
-    "amdgpu"
-    "nvidia"
-  ];
+{config, ...}: {
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = ["nvidia" "modesetting"];
   services.hardware.bolt.enable = true;
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
-    powerManagement.finegrained = true;
-    open = false;
+    open = true;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
   };
 }
