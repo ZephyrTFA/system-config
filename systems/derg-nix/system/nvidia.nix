8617e2c65ecaced4a398a2815364e81f98bf9a7a
ZephyrTFA (Matthew) <matthew@tfaluc.com>
2025-08-25 14:44:50 -0400

    swap to vulkan, again
    also flake update

--- systems/derg-nix/system/nvidia.nix
+++ systems/derg-nix/system/nvidia.nix
@@ -3,10 +3,11 @@
   services.xserver.videoDrivers = ["nvidia" "modesetting"];
   services.hardware.bolt.enable = true;
   hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
     modesetting.enable = true;
     powerManagement.enable = true;
     open = true;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 }
+
