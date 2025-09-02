3fce7424df81f165243fb9643f5a0b1043d61733
ZephyrTFA (Matthew) <matthew@tfaluc.com>
2025-09-02 11:18:57 -0400

    enable opengl also 32bit support

--- systems/derg-nix/system/nvidia.nix
+++ systems/derg-nix/system/nvidia.nix
@@ -1,7 +1,20 @@
 {config, ...}: {
-  hardware.graphics.enable = true;
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  # you would think these are enable by default.
+  hardware.opengl = {
+    enable = true;
+    driSupport32Bit = true;
+  };
+
+  # explicitly tell xserver to use nvidia over software rendering
   services.xserver.videoDrivers = ["nvidia"];
+  # text based mode needs the same hint
   boot.initrd.kernelModules = ["nvidia"];
+
   boot.extraModulePackages = [config.boot.kernelPackages.nvidia_x11];
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
